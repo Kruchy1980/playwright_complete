3d8b11b70af3c56f8eb63c1943c1cd0f08c5a82e
Kruchy1980 <jakub.kruszynski@vp.pl>
2023-09-30 17:14:56 +0200

    The part of the xpath selectors used as locator for specific element

---
+++ my_tests/7.Xpath_selector.spec.ts
@@ -0,0 +1,30 @@
+// THIS PART IS DEDICATED FOR USING XPATH SELECTOR IN PLAYWRIGHT
+//The Page URL Main Address: "https://practice.sdetunicorns.com/"
+// Firstly import the needed modules from playwright
+import {
+  expect,
+  test,
+} from '@playwright/test';
+
+// Here we are starting logic for tests
+test.describe('Usage of xpat selector', () => {
+   // Start the test in here
+   test('Xpath - selector usage - search icon displayed', async ({ page }) => {
+      // 1. Go to proper page
+      await page.goto('https://practice.sdetunicorns.com/');
+      // 2. Select element "search-icon" on the page using xpath selector
+      await page.locator('//*[@class="zakra-icon--magnifying-glass"]').isVisible();
+      // 3. Other verification of displayed element - variable preparation
+      const sectionTitle = await page.locator('//*[@id="primary"]/div/section[1]/div/div/div/div[1]/div/h3/div/h2/span');
+      // 4. Assertion for element with xpath locator used
+      await expect(sectionTitle).toBeVisible();
+      // !!5. The problem with search icon solution
+      // const searchIcon = await page.locator('.zakra-icon--magnifying-glass').isVisible();
+      const searchIcon = await page.locator('#zak-masthead > div > div > div > div.zak-header-col.zak-header-col--2 > div.zak-header-actions.zak-header-actions--desktop > div.zak-header-action.zak-header-search > a > svg') //.isVisible();
+      // II6. Assertion try
+      await expect(searchIcon).toBeVisible();
+   })
+});
+/*
+There is problem with assertion for xpath to be used for the icon visibility
+*/
