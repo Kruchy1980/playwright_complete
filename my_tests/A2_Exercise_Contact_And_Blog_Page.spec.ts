7f87278e4aab5564afb3724fc324ddce9fe01b06
Kruchy1980 <jakub.kruszynski@vp.pl>
2023-09-30 19:38:18 +0200

    The Exercise points to be verified description added to the Ex 2 file

--- my_tests/A2_Exercise_Contact_And_Blog_Page.spec.ts
+++ my_tests/A2_Exercise_Contact_And_Blog_Page.spec.ts
@@ -1,10 +1,15 @@
 // EXERCISED: CONTACG & BLOG PAGE
 //The Page URL Main Address: "https://practice.sdetunicorns.com/"
 // Firstly import the needed modules from playwright
-/*
+/* First Contact Page:
 In this exercise the steps to be done.
 1- Access Contact Page
 2- Fill the form
 3. Submit the form
 4. Assert the success message " Thanks for contacting us! We will be in touch with you shortly " 
+
+Second Blog Page:
+1. Go to Blog Page
+2. Get length of list of Recnt Posts - 5 elements
+3. For each individual link/text  create assertion that each of them char length >10
 */
