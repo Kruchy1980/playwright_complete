76d45b719c2872cb3872ddaacd34404a4e1c0b4d
Kruchy1980 <jakub.kruszynski@vp.pl>
2023-10-16 15:53:26 +0200

    The part of the methods separation added

--- pages/1.home.page.ts
+++ pages/1.home.page.ts
@@ -1,26 +1,30 @@
 // THE FILE DEDICATED FOR STORING ALL LOCATORS/SELECTORS AND HELP METHODS ONLY.
 
-import { Page } from '@playwright/test';
+import {
+  Locator,
+  Page,
+} from '@playwright/test'; // !!!! To fix the autocompletion from this class we need to add Loctor from the same playwright module that the framework will see the 
 
 //1 Create the class
 class HomePage {
     // Declare property page (as in ts should be done)
     // Because of using TypeScript we need to assign all the types of vqariables used in here
-    page: any; // The page can consists any elemnet to be visible ion the page
-    getStartedBtn: any;
-    headingText: any;
-    homeLink: any;
-    searchIcon: any;
-    navLink: any;
-    cSensitiveHeadingText: any;
-    homeText: any;
-    homeTextAlternate: any;
-    homeTextAlternate_two: any;
-    homeText_1: any;
-    sectionTitle: any;
-    searchIconAlt: any;
+    // The page can consists any elemnet to be visible ion the page
+    page: Page; // !!! Now we need ot change all of the locators to proper module name then we need to go to our terst file and add proper property to eaCH VARIAvle declarartion
+    getStartedBtn: Locator;
+    headingText: Locator;
+    homeLink: Locator;
+    searchIcon: Locator;
+    navLink: Locator;
+    cSensitiveHeadingText: Locator;
+    homeText: Locator;
+    homeTextAlternate: Locator;
+    homeTextAlternate_two: Locator;
+    homeText_1: Locator;
+    sectionTitle: Locator;
+    searchIconAlt: Locator;
     // 2. Ccreate a constructor in the class which will store all of the needed locators - remember we arew using typescript here
-    constructor(page: Page) {
+    constructor(page: Page) { // !!! Using Typescript we need to add the type of parameter in constructor as well
         this.page = page; // the constructor for identify the page in the tests
         // Here we can assign other locators for the class - title variable
         // In the constructor we can use locators as well ex
@@ -40,6 +44,14 @@ class HomePage {
         this.sectionTitle = page.locator('//*[@id="primary"]/div/section[1]/div/div/div/div[1]/div/h3/div/h2/span');
         //======== The whole part above - the constructor is the bqasic page templatge and this is something that we are going to use for all the page files that we are going to be created. 
     };
+    // Adding methods in here as asybnjchronous method for navigation
+    async navigate() {
+        await this.page.goto('https://practice.sdetunicorns.com/'); 
+    }
+    // Getting navLinks Texts
+    async getNavLinksText() {
+        return this.navLink.allInnerTexts();
+    }
 };
 
 // Export the class to be visible from other folders
