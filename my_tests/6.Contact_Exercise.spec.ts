b20012831b6b5251ab0a13462b9d29487064c6bf
Kruchy1980 <jakub.kruszynski@vp.pl>
2023-10-22 11:23:24 +0200

    The part of codes for optimization added

---
+++ my_tests/6.Contact_Exercise.spec.ts
@@ -0,0 +1,33 @@
+//THE CIBTACT PAGE TESTS PREPARATION  - Make component for this code
+// URL used: "https://practice.sdetunicorns.com/contact"
+
+import {
+  expect,
+  test,
+} from '@playwright/test';
+
+test.describe('Contact', () => {
+  test('Fill contact form and verify success message', async ({ page }) => {
+    // open contact page
+    await page.goto('https://practice.sdetunicorns.com/contact')
+
+    //  fill out the input fields
+    await page.locator('.contact-name input').fill('Test Name')
+    await page.locator('.contact-email input').fill('test@mail.com')
+    await page.locator('.contact-phone input').fill('134567864')
+    await page.locator('.contact-message textarea').fill('This is a test message')
+
+    // add a soft assertion
+    await expect.soft(page.locator('.contact-message textarea')).toHaveText("Fail test message")
+
+    // click submit
+    await page.locator('button[type=submit]').click()
+
+    expect(test.info().errors.length).toBeLessThan(1)
+
+    // verify success message
+    const successAlert = page.locator('div[role="alert"]')
+    await expect(successAlert).toHaveText('for contacting us! We will be in touch with you shortly')
+  });
+
+});
